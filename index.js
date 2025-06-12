8918a2b1f9c4fdc2a284fa23667906fb3856cebb
Mithun Yadav <mithuny1998@gmail.com>
2025-06-12 07:12:56 +0530

    🎉 Add initial HTML, CSS, and JavaScript files for project setup

---
+++ index.js
@@ -0,0 +1,34 @@
+/********************************************************************/
+// first question convert array into object and with count of each item
+/*
+const arr = [
+  "a",
+  "b",
+  "c",
+  "a",
+  "d",
+  "b",
+  "c",
+  "a",
+  "e",
+  "f",
+  "g",
+  "b",
+  "f",
+  "h",
+];
+
+function countItem(arr) {
+  // const arr = ["a","b","c"];
+  let obj = {};
+  arr.forEach((item) => {
+    obj[item] = (obj[item] || 0) + 1;
+  });
+
+  return obj;
+}
+
+const result = countItem(arr);
+console.log(result); //{ a: 3, b: 3, c: 2, d: 1, e: 1, f: 2, g: 1, h: 1 }
+*/
+/********************************************************************/
